7e2692702523109a364010efd825115ba81169d2
aloner778 <aloner8888@gmail.com>
2020-06-15 12:34:01 +0900

    display all operation logs

--- src/components/OperationLogs.js
+++ src/components/OperationLogs.js
@@ -1,29 +1,26 @@
 import React, { useContext } from 'react'
 
-import Event from './Event'
+import OperationLog from './OperationLog'
 import AppContext from '../contexts/AppContext'
 
-const Events = () => {
+const OperationLogs = () => {
   const { state } = useContext(AppContext)
-  
   return (
     <>
-      <h4>イベント一覧</h4>
+      <h4>操作ログ一覧</h4>
       <table className="table table-hover">
         <thead>
           <tr>
-            <th>ID</th>
-            <th>タイトル</th>
             <th>内容</th>
-            <th></th>
+            <th>日時</th>
           </tr>
         </thead>
         <tbody>
-          { state.events.map((event, index) => (<Event key={index} event={event} />)) }
+          {state.operationLogs.map((operationLog, index) => { return <OperationLog key={index} operationLog={operationLog} /> })}
         </tbody>
       </table>
     </>
   )
 }
 
-export default Events
+export default OperationLogs
